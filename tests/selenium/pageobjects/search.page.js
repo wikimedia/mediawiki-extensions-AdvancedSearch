7121fe8c295238b4d50c1fd30f0b20fb00155719
Gabriel Birke <gabriel.birke@wikimedia.de>
2018-01-24 17:25:18 +0100

    Check search form state after submission
    Automatically wait for advanced search to be rendered after opening the
    search page.
    Bug: T167316
    
    Change-Id: I057336a52795e61e7e17b1992a5e25b7b192ff2c

--- tests/selenium/pageobjects/search.page.js
+++ tests/selenium/pageobjects/search.page.js
@@ -11,8 +11,16 @@ class SearchPage extends Page {
 	get searchNotTheseWordsTagLabel() { return browser.element( '#advancedSearchOption-not .oo-ui-tagItemWidget > .oo-ui-labelElement-label' ); }
 	get searchOneWord() { return browser.element( '#advancedSearchOption-or' ); }
 	get searchOneWordTagLabel() { return browser.element( '#advancedSearchOption-or .oo-ui-tagItemWidget > .oo-ui-labelElement-label' ); }
-	get searchExpandablePane() { return browser.element( '.mw-advancedSearch-expandablePane' ); }
+	get searchExpandablePane() { return browser.element( '.mw-advancedSearch-expandablePane-button' ); }
+	get searchPreview() { return browser.element( '.mw-advancedSearch-searchPreview' ); }
+	get searchPreviewItems() { return browser.elements( '.mw-advancedSearch-searchPreview .mw-advancedSearch-searchPreview-previewPill' ); }
+
 	formWasSubmitted() { return browser.getUrl().match( /\?advancedSearchOption-original=/ ) !== null; }
+
+	advancedSearchIsCollapsed() {
+		return browser.element( '.mw-advancedSearch-expandablePane > .oo-ui-indicatorElement .oo-ui-indicatorElement-indicator.oo-ui-indicator-down' ).isExisting();
+	}
+
 	getSearchURL() {
 		let search = browser.getUrl().split( '&' ).filter( function ( part ) {
 			return part.match( /^search=/ );
@@ -22,7 +30,16 @@ class SearchPage extends Page {
 
 	open() {
 		super.open( 'Special:Search' );
+		this.waitForAdvancedSearchToLoad();
 	}
 
+	waitForAdvancedSearchToLoad() {
+		browser.waitForVisible( '.mw-advancedSearch-container', 5000 );
+	}
+
+	submitForm() {
+		browser.element( '#mw-search-top-table button' ).click();
+		this.waitForAdvancedSearchToLoad();
+	}
 }
 module.exports = new SearchPage();
---
+++ tests/selenium/specs/search_submit.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var assert = require( 'assert' );
+var SearchPage = require( '../pageobjects/search.page' );
+
+describe( 'Advanced Search submit', function () {
+
+	it( 'no search preview is shown in expanded state', function () {
+		SearchPage.open();
+		SearchPage.searchExpandablePane.click();
+		SearchPage.searchTheseWords.click();
+		browser.keys( 'test,' );
+		SearchPage.searchNotTheseWords.click();
+		browser.keys( 'test3 ' );
+		SearchPage.searchOneWord.click();
+		browser.keys( 'test4 test5' );
+		SearchPage.searchExactText.setValue( '"test1 test2"' );
+
+		assert( !SearchPage.searchPreviewItems.isExisting(), 'No preview pill elements should exist' );
+
+	} );
+
+	it( 'shows search preview in collapsed state', function () {
+		SearchPage.open();
+		SearchPage.searchExpandablePane.click();
+		SearchPage.searchTheseWords.click();
+		browser.keys( 'test,' );
+		SearchPage.searchNotTheseWords.click();
+		browser.keys( 'test3 ' );
+		SearchPage.searchOneWord.click();
+		browser.keys( 'test4 test5' );
+		SearchPage.searchExactText.setValue( '"test1 test2"' );
+		SearchPage.searchExpandablePane.click();
+		browser.waitUntil( SearchPage.advancedSearchIsCollapsed, 5000 );
+
+		assert( SearchPage.searchPreviewItems.isExisting(), 'Preview pills should be shown' );
+		assert.strictEqual( SearchPage.searchPreviewItems.value.length, 4, 'Number of preview pills must match number of filled fields' );
+	} );
+
+	it( 'collapses search preview after submission', function () {
+
+		SearchPage.open();
+		SearchPage.searchExpandablePane.click();
+		SearchPage.searchTheseWords.click();
+		browser.keys( 'test,' );
+		SearchPage.submitForm();
+
+		assert( SearchPage.advancedSearchIsCollapsed() );
+
+	} );
+
+} );
