14360f24dbc7bedf4c7e396d60ffb110604fc8dd
aniansson <ebba.aniansson@wikimedia.de>
2018-03-07 18:19:21 +0100

    Change the default file option text
    Bug: T181583
    Change-Id: I982305fc7ced4231cc0903dde16689fd388fad8d

--- modules/ui/ext.advancedSearch.FileTypeSelection.js
+++ modules/ui/ext.advancedSearch.FileTypeSelection.js
@@ -6,7 +6,7 @@
 	mw.libs.advancedSearch.ui = mw.libs.advancedSearch.ui || {};
 
 	var getOptions = function ( optionProvider ) {
-		return [ { data: '', label: '' } ]
+		return [ { data: '', label: mw.msg( 'advancedsearch-filetype-default' ) } ]
 			.concat( optionProvider.getFileGroupOptions() )
 			.concat( optionProvider.getAllowedFileTypeOptions() );
 	};
