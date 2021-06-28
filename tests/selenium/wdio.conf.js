70d9b1339141b41687c92fe578b3346a9c70d393
WMDE-Fisch <christoph.jauera@wikimedia.de>
2021-06-28 10:11:20 +0200

    selenium: Re-add daily suite
    Accidentally removed in  Ie3fb2673f6bc9c48410afc3d83a083f0017fbc7c
    
    Change-Id: I979703ca030c2480bf74798422f5c65fbae7e18b

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -2,10 +2,19 @@
 
 const { config } = require( 'wdio-mediawiki/wdio-defaults.conf.js' );
 
-exports.config = { ...config
+exports.config = { ...config,
 	// Override, or add to, the setting from wdio-mediawiki.
 	// Learn more at https://webdriver.io/docs/configurationfile/
 	//
 	// Example:
 	// logLevel: 'info',
+
+	suites: {
+		daily: [
+			'./tests/selenium/specs/defaultNamespaces.js',
+			'./tests/selenium/specs/paginationLinks.js',
+			'./tests/selenium/specs/searchPage.js',
+			'./tests/selenium/specs/searchSubmit.js'
+		]
+	}
 };
