99611d0426d2b6ee6a622b8cfad5c91cd9571413
thiemowmde <thiemo.kreuz@wikimedia.de>
2023-09-11 15:40:22 +0200

    Dramatically simplify some older PHPUnit tests
    Some stuff was tested multiple times, and the setup was quite
    overcomplicated, in my opinion.
    
    Change-Id: I78baf3f8b4625ae19903fbdb9d5d46cfc52a2dae

--- tests/phpunit/MimeTypeConfiguratorTest.php
+++ tests/phpunit/MimeTypeConfiguratorTest.php
@@ -13,49 +13,29 @@ use PHPUnit\Framework\TestCase;
  */
 class MimeTypeConfiguratorTest extends TestCase {
 
-	private const EXT_SINGLE_MIMETYPE = 'singletype';
-	private const EXT_MULTIPLE_MIMETYPES = 'multitype';
-	private const EXT_SAME_MIMETYPE = 'same1';
-	private const OTHER_EXT_SAME_MIMETYPE = 'same2';
+	public function testGetMimeTypes() {
+		$fileExtensions = [
+			// Expected to skip unknown extensions
+			'unknown',
+			// Expected to stick to the first extension when they have the same MIME type
+			'jpg',
+			'jpeg',
+		];
+		$fileExtensionToMimeTypeMap = [
+			// The order here doesn't matter, it's just a lookup map
+			'jpg' => 'MIME/jpeg',
+			'jpeg' => 'MIME/jpeg',
+		];
+		$expected = [
+			'jpg' => 'MIME/jpeg',
+		];
 
-	public function testConfiguratorReturnsMimeTypeForFileExtension() {
-		$configurator = new MimeTypeConfigurator( $this->newMimeAnalyzerMock() );
-		$this->assertSame(
-			[ self::EXT_SINGLE_MIMETYPE => 'test' ],
-			$configurator->getMimeTypes( [ self::EXT_SINGLE_MIMETYPE ] )
-		);
-	}
+		$analyzer = $this->createMock( MimeAnalyzer::class );
+		$analyzer->method( 'getMimeTypeFromExtensionOrNull' )
+			->willReturnCallback( fn ( $ext ) => $fileExtensionToMimeTypeMap[$ext] ?? null );
 
-	public function testConfiguratorAlwaysUsesTheFirstMimeType() {
-		$configurator = new MimeTypeConfigurator( $this->newMimeAnalyzerMock() );
-		$this->assertSame(
-			[ self::EXT_MULTIPLE_MIMETYPES => 'one' ],
-			$configurator->getMimeTypes( [ self::EXT_MULTIPLE_MIMETYPES ] )
-		);
-	}
-
-	public function testConfiguratorDoesNotAllowRedundantEntries() {
-		$configurator = new MimeTypeConfigurator( $this->newMimeAnalyzerMock() );
-		$this->assertSame(
-			[ self::EXT_SAME_MIMETYPE => 'mime' ],
-			$configurator->getMimeTypes( [ self::EXT_SAME_MIMETYPE, self::OTHER_EXT_SAME_MIMETYPE ] )
-		);
-	}
-
-	/**
-	 * @return MimeAnalyzer
-	 */
-	private function newMimeAnalyzerMock() {
-		$mock = $this->createMock( MimeAnalyzer::class );
-		$mock->method( 'getMimeTypeFromExtensionOrNull' )->will(
-			$this->returnValueMap( [
-				[ self::EXT_SINGLE_MIMETYPE, 'test' ],
-				[ self::EXT_MULTIPLE_MIMETYPES, 'one' ],
-				[ self::EXT_SAME_MIMETYPE, 'mime' ],
-				[ self::OTHER_EXT_SAME_MIMETYPE, 'mime' ]
-			] )
-		);
-		return $mock;
+		$configurator = new MimeTypeConfigurator( $analyzer );
+		$this->assertSame( $expected, $configurator->getMimeTypes( $fileExtensions ) );
 	}
 
 }
