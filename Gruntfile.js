5a62261e6a2e35632e761420c6b56d498c9a0671
James D. Forrester <jforrester@wikimedia.org>
2023-09-18 19:40:12 +0100

    build: Use conf.MessageDirs for banana path config
    Rather than configuring manually, re-use the specification in the
    repo's extension.json file, like we do elsewhere; this way, if we
    add further i18n files in future, or change their paths, we won't
    need to update this configuration.
    
    Change-Id: I5969d576a78efb1095f2c8c68a0700b621585b89

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function ( grunt ) {
+	const conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
@@ -24,9 +26,7 @@ module.exports = function ( grunt ) {
 				'!vendor/**'
 			]
 		},
-		banana: {
-			all: 'i18n/'
-		}
+		banana: conf.MessagesDirs
 	} );
 
 	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
