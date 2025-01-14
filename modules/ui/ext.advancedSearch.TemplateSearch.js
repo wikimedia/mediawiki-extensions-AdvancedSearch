f318ec760dd464d73099a019ca59c45929051890
thiemowmde <thiemo.kreuz@wikimedia.de>
2025-01-14 10:27:39 +0100

    Remove duplicate code from MultiselectLookup subclasses
    The MultiselectLookup base class already does this. This duplication
    is meaningless and confusing. The code was left behind when the base
    class was introduced via I17a3016. The two subclasses already existed
    at this point.
    
    We could even remove the two subclasses and turn the very few
    remaining differences into configuration. However, I would like to
    do this after I851452d and keep the two subclasses for the moment.
    
    Change-Id: If546481ed0dcbbe739df58e9c8cbeef4f92e496c

--- modules/ui/ext.advancedSearch.TemplateSearch.js
+++ modules/ui/ext.advancedSearch.TemplateSearch.js
@@ -11,32 +11,11 @@ const MultiselectLookup = require( '../dm/ext.advancedSearch.MultiselectLookup.j
  * @param {Object} config
  */
 const TemplateSearch = function ( store, config ) {
-	this.store = store;
-
 	TemplateSearch.super.call( this, store, config );
 
 	this.$element.addClass( 'mw-advancedSearch-template' );
-
-	this.populateFromStore();
 };
 
 OO.inheritClass( TemplateSearch, MultiselectLookup );
-
-TemplateSearch.prototype.onStoreUpdate = function () {
-	this.populateFromStore();
-};
-
-TemplateSearch.prototype.populateFromStore = function () {
-	if ( this.store.hasFieldChanged( this.fieldId, this.getValue() ) ) {
-		this.setValue( this.store.getField( this.fieldId ) );
-	}
-};
-
-/**
- * Update external states on internal updates
- */
-TemplateSearch.prototype.onValueUpdate = function () {
-	this.store.storeField( this.fieldId, this.getValue() );
-};
 
 module.exports = TemplateSearch;
