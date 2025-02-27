55f34c794bbef7f17969ceab86f46dcd4c3d7905
Ed Sanders <esanders@wikimedia.org>
2025-02-27 13:45:05 +0000

    Remove unnecessary context binding
    Change-Id: I05e11fba0c52bed2ef550c98599c702e658b2597

--- modules/dm/ext.advancedSearch.NamespacePresetProviders.js
+++ modules/dm/ext.advancedSearch.NamespacePresetProviders.js
@@ -46,13 +46,11 @@ NamespacePresetProviders.prototype.hasProvider = function ( providerName ) {
  * @return {string[]}
  */
 NamespacePresetProviders.prototype.getNamespaceIdsFromProvider = function ( providerName ) {
-	const self = this;
-
 	return this.providerFunctions[ providerName ]( Object.keys( this.namespaces ) )
 		// Calling String() as a function casts numbers to strings
 		.map( String )
 		.filter( ( id ) => {
-			if ( id in self.namespaces ) {
+			if ( id in this.namespaces ) {
 				return true;
 			}
 			mw.log.warn( 'AdvancedSearch namespace preset provider "' + providerName + '" returned invalid namespace id' );
